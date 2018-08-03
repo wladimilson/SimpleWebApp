b5cab39ee8c89cc72b3f92096e498998ebeb4c97
Wladimilson <wladimilsonmn@gmail.com>
2018-08-03 14:13:15 -0300

    Banco de dados

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Net;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -8,11 +9,18 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Data.Sqlite;
+using System.Text;
 
 namespace WebApp
 {
     public class Program
     {
+        public struct Article
+        {
+            public string Title;
+            public string Content;
+        }
         public static void Main(string[] args)
         {
             
@@ -21,10 +29,44 @@ namespace WebApp
                 {
                     app.Run(async (context) =>
                     {
-                        await context.Response.WriteAsync("Bem-vindo ao site!");
+                        
+                        context.Response.ContentType = "text/html; charset=utf-8";
+
+                        var content = new StringBuilder("<dl>");
+                        foreach(var article in Articles())
+                            content.Append($"<dt>{article.Title}</dt><dd>{article.Content}</dd>");
+                        content.Append("</dt>");
+
+                        await context.Response.WriteAsync(content.ToString());
                     });
                 })
                 .Build().Run();
+        }
+
+        public static List<Article> Articles() {
+            var articles = new List<Article>();
+            using (var connection = new SqliteConnection(new SqliteConnectionStringBuilder
+                        {
+                            DataSource = "data.db"
+                        }.ToString()))
+                {
+                
+                    connection.Open();
+
+                    var command = connection.CreateCommand();
+                    command.CommandText = "SELECT * FROM Article";
+                    using (var reader = command.ExecuteReader())
+                    {
+                        Console.WriteLine(reader.RecordsAffected);
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(reader.ToString());
+                            articles.Add(new Article { Title = reader["title"].ToString(), Content = reader["content"].ToString()});
+                        }
+                    }
+            }
+
+            return articles;
         }
     }
 }
